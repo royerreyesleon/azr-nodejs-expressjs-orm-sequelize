8e7113a91ffc477a2a02fdf3ee2527b22a37e5e9
Royer Reyes León <royer.stecnica@gmail.com>
2021-04-20 05:34:32 -0500

    Agregando cliente que se comunica con la API REST

--- config.js
+++ config.js
@@ -1,7 +1,17 @@
 module.exports = {
     database: {
+    	// MYSQL
         username:"root",
         password:"1234",
+
+        // POSTGRESQL
+        // username:"postgres",
+        // password:"1234",
+
+        // SQLSERVER
+        // username:"root",
+        // password:"1234",
+
         database:"company",
         host:"localhost"
 
